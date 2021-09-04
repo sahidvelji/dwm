de74f98f4c77a3b9be1c7a153b88a71a8cbe46af
Sahid Velji <sahidvelji@gmail.com>
2021-09-04 19:47:52 -0400

    Bind uoft login

--- config.h
+++ config.h
@@ -200,7 +200,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_l,		setmfact,      	{.f = +0.05} },
 	{ MODKEY,			XK_semicolon,	shiftview,	{ .i = 1 } },
 	{ MODKEY|ShiftMask,		XK_semicolon,	shifttag,	{ .i = 1 } },
-	/* { MODKEY,			XK_apostrophe,	togglescratch,	{.ui = 1} }, */
+	{ MODKEY,			XK_apostrophe,	spawn,	SHCMD("uoftlogin") },
 	/* { MODKEY|ShiftMask,		XK_apostrophe,	spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
 	/* { MODKEY|ShiftMask,		XK_Return,	togglescratch,	{.ui = 0} }, */
